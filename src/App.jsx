8f8824eee87625bbafe110a35f9bbe4f731da94f
NonTecyUser <samiraryal449@gmail.com>
2026-02-15 00:04:56 +0545

    fixed images path issue:

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,11 @@
 
 import Entry from "./component/Entry.jsx";
 import Header from "./component/Header.jsx";
+import mountEverest from "./assets/mountEverest.png"; 
+import mountFuji from "./assets/mountFuji.png";
+import waterfall from "./assets/waterfall.png";
+import desert from "./assets/desert.png";    
+
 function App() {
   return (
   <div className= "Journal">
@@ -9,7 +14,7 @@ function App() {
    
       {/* //entry 1 */}
       <Entry
-       image={{src:"./src/assets/mountEverest.png",alt:"mount fuji"}}
+       image={{src:mountEverest,alt:"mount everest"}}
        topDetail={{country:"Nepal",
          mapLink:"https://www.google.com/maps/place/Mt+Everest/@27.9881503,86.8841669,13z/data=!3m1!4b1!4m6!3m5!1s0x39e854a215bd9ebd:0x576dcf806abbab2!8m2!3d27.9881569!4d86.9253667!16zL20vMGJsYmQ?entry=ttu&g_ep=EgoyMDI2MDExMy4wIKXMDSoKLDEwMDc5MjA3M0gBUAM%3D"
        }}
@@ -20,7 +25,7 @@ function App() {
 
     {/* //entry 2 */}
    <Entry
-    image={{src:"./src/assets/mountFuji.png",alt:"mount fuji"}}
+    image={{src:mountFuji,alt:"mount fuji"}}
     topDetail={{country:"japan",
       mapLink:"https://www.google.com/maps/place/Mount+Fuji/@35.360625,138.7273634,14z/data=!3m1!4b1!4m6!3m5!1s0x6019629a42fdc899:0xa6a1fcc916f3a4df!8m2!3d35.3606255!4d138.7273634!16zL20vMGNrczA?entry=ttu&g_ep=EgoyMDI2MDExMy4wIKXMDSoKLDEwMDc5MjA3M0gBUAM%3D"
     }}
@@ -32,7 +37,7 @@ function App() {
 
    {/* //entry 3 */}
    <Entry
-    image={{src:"./src/assets/waterfall.png",alt:"Iguazu Waterfall"}}
+    image={{src:waterfall,alt:"Iguazu Waterfall"}}
     topDetail={{country:"Argentina-Brazil Border",
       mapLink:"https://www.google.com/maps/place/Igua%C3%A7u+National+Park/@-25.5715796,-54.7923267,9z/data=!4m10!1m2!2m1!1siguazu+falls+brazil!3m6!1s0x94f14d6db34e243d:0x6e633cd35854aa8f!8m2!3d-25.460508!4d-53.8177841!15sChNpZ3VhenUgZmFsbHMgYnJhemlsWhUiE2lndWF6dSBmYWxscyBicmF6aWySAQ1uYXRpb25hbF9wYXJrmgEkQ2hkRFNVaE5NRzluUzBWSlEwRm5TVVJLTnpodU1USkJSUkFC4AEA-gEECB4QSg!16zL20vMDRqMjVn?entry=ttu&g_ep=EgoyMDI2MDExMy4wIKXMDSoKLDEwMDc5MjA3M0gBUAM%3D"
     }}
@@ -44,7 +49,7 @@ function App() {
 
    {/* //entry  4*/}
    <Entry
-    image={{src:"./src/assets/desert.png",alt:"Grand Canyon"}}
+    image={{src:desert,alt:"Grand Canyon"}}
     topDetail={{country:"USA ",
       mapLink:"https://www.google.com/maps/place/Grand+Canyon/@36.0997631,-112.1124846,3a,84.3y,90t/data=!3m8!1e2!3m6!1sCIABIhBRtSpKDnzSOyhi4p-eVITv!2e10!3e12!6shttps:%2F%2Flh3.googleusercontent.com%2Fgps-cs-s%2FAG0ilSxkUi7ifurGOUwR_EPl-9j8Xre8sga3YJ7o19ujgNBP5LPIglp7r85M6O8CMxmgJ-wEW7aw8l494h506a4PUNSUYT0cIqz1uuGYYAIQMMds8M2KKyYnzDll9BZVvfEuEBdC0M7LEWt0kN9E%3Dw203-h135-k-no!7i612!8i408!4m9!3m8!1s0x80cc0654bd27e08d:0xb1c2554442d42e8d!8m2!3d36.0997631!4d-112.1124846!10e5!14m1!1BChAqDC9nLzExY2Zmd3JocjAL!16zL20vMGNuczU?entry=ttu&g_ep=EgoyMDI2MDExMy4wIKXMDSoKLDEwMDc5MjA3M0gBUAM%3D"
     }}
--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -1,12 +1,16 @@
-function Header() {
+import react from "react";
+
+import globeimg from "../assets/globe.png";
+function Header() { 
   return (
    <header>
         <div className = "container">
-        <img src = "globe.png" alt= "globe-img"/>
+        {/* <img src = "globe.png" alt= "globe-img"/> */}
+        <img src = {globeimg} alt= "globe-img"/>
         <p>my traveling journal</p>
         </div>
        
   
 </header>)
 }
-    export default Header
+    export default Header 
